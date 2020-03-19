741d4dd72b0fab0d08e4816ac7b9ce595e5f8045
CBreakr <github email address>
2020-03-18 20:23:19 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :artists, except: [:delete]
+  resources :songs, except: [:delete]
+  resources :genres, except: [:delete]
 end
