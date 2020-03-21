fd2ffacaf144a5972116bc4ec6b36623a5e912a5
theslickestnick <nick.echev@gmail.com>
2020-03-20 20:39:50 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,30 @@
+class SongsController < ApplicationController
+
+    def index 
+        @songs = Song.all
+    end
+
+    def show
+        @song = Song.find(params[:id])
+        @genre = Genre.find((params[:id]))
+    end
+
+    def new 
+        @song = Song.new
+    end
+
+    def create 
+        @song = Song.create(song_params)
+        redirect_to song_path(@song)
+    end
+
+    def edit
+        @song = Song.find(params[:id])
+    end
+
+    private 
+
+    def song_params
+        params.require(:song).permit!
+    end
+end
